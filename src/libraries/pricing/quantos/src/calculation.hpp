f2b7d454eb0f8df208a3c8169ea85cfa5345b75c
Stewart Henderson <shenderson@mozilla.com>
2020-02-04 08:05:28 -0600

    Setup of Quantos pricing

---
+++ src/libraries/pricing/quantos/src/calculation.hpp
@@ -0,0 +1,13 @@
+// Copyright 2019 Stewart Henderson. All rights reserved.
+
+#pragma once
+#ifndef __QUANTOS_CALCULATION__
+#define __QUANTOS_CALCULATION__
+
+namespace trading {
+namespace pricing {
+namespace quantos {}
+}  // namespace pricing
+}  // namespace trading
+
+#endif  //__QUANTOS_CALCULATION__
