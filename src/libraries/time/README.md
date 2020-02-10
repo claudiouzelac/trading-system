1a75bc6cc74b026b85d67537319c5981a5aa0ae5
Stewart Henderson <shenderson@mozilla.com>
2020-02-09 18:35:05 -0600

    added calendar object

---
+++ src/libraries/time/README.md
@@ -0,0 +1,8 @@
+# Time Libraries
+
+This directory contains libraries for interacting with time and dates for pricing
+instrument related objects.
+
+## Author(s)
+
+Stewart Henderson
---
+++ src/libraries/time/calendar/inc/calendar.h
@@ -0,0 +1,24 @@
+// Copyright 2019 Stewart Henderson. All rights reserved.
+
+#pragma once
+#ifndef __CALENDAR__
+#define __CALENDAR__
+
+#include <string>
+
+#include "boost/date_time/gregorian/gregorian.hpp"
+
+// Boost Calendar Example
+// https://www.boost.org/doc/libs/1_31_0/libs/date_time/doc/exp-print_holidays.html
+
+namespace libraries {
+namespace time {
+namespace calendar {
+class Calendar {
+ public:
+};
+}  // namespace calendar
+}  // namespace time
+}  // namespace libraries
+
+#endif
